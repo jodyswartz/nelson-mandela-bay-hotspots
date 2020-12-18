9dc944839961b8824d3db8a5727f80bd8cf27980
Jody Swartz <jodyhswartz@hotmail.com>
2020-12-18 17:09:09 +0100

    Minor changes to Date

--- src/components/CovidMap.jsx
+++ src/components/CovidMap.jsx
@@ -53,12 +53,12 @@ const CovidMap = ({ countries }) => {
       />
        <Control position="topright">
          <Paper style={{padding: '20px'},{backgroundColor: fade('#484848', 0.5)}}>
-           <h2><font color="#f8f8ff">Active Cases <b>2209</b> Updated: <b>10-12-2020</b></font></h2>        
+           <h2><font color="#f8f8ff">Active Cases <b>2209</b> Updated: <b>13-12-2020</b></font></h2>        
          </Paper>
    </Control>
    <Control position="bottomright">
        <Paper style={{padding: '20px'},{backgroundColor: fade('#484848', 0.5)}}>
-       <font color="#f8f8ff"><h5>Active 10 December 2020</h5></font>  
+       <font color="#f8f8ff"><h5>Active 13 December 2020</h5></font>  
             <font color="#f8f8ff">
               <p><b>209</b><h6> Unknown Location</h6></p>
               <p><b>169</b><h6> New Cases</h6></p>
