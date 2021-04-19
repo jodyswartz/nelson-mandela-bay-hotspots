d8bf2ec15b185d8fa38c91f0a6defbcba46497b1
Jody Swartz <jodyhswartz@hotmail.com>
2021-04-19 21:23:44 +0200

    Updates

---
+++ precache-manifest.415eefb6fc318790e48023b3decce24e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d6bd42ac9ecff77bfeb",
+    "url": "/nelson-mandela-bay-hotspots/static/css/main.34396e1b.chunk.css"
+  },
+  {
+    "revision": "1d6bd42ac9ecff77bfeb",
+    "url": "/nelson-mandela-bay-hotspots/static/js/main.690e7a06.chunk.js"
+  },
+  {
+    "revision": "2c9b747c69d3b0dd4497",
+    "url": "/nelson-mandela-bay-hotspots/static/js/runtime~main.a12ae2b1.js"
+  },
+  {
+    "revision": "374c28ad6c67adb2f212",
+    "url": "/nelson-mandela-bay-hotspots/static/css/2.c7fa865a.chunk.css"
+  },
+  {
+    "revision": "374c28ad6c67adb2f212",
+    "url": "/nelson-mandela-bay-hotspots/static/js/2.ef776ab6.chunk.js"
+  },
+  {
+    "revision": "4864cd0f3e704c4e2faa9c212536365f",
+    "url": "/nelson-mandela-bay-hotspots/index.html"
+  }
+];
