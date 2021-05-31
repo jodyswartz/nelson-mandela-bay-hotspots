af8e08ae9be73419a334581a742b39f2b2102a00
Jody Swartz <jodyhswartz@hotmail.com>
2021-05-31 19:37:55 +0200

    Updated data 30/05/2021

--- src/components/CovidMap.jsx
+++ src/components/CovidMap.jsx
@@ -55,17 +55,17 @@ const CovidMap = ({ wards }) => {
       />
        <Control position="topright">
          <Paper style={{padding: '20px'},{backgroundColor: fade('#484848', 0.5)}}>
-           <h2><font color="#f8f8ff">Covid-19 Active Cases <b>399</b> Updated: <b>27-05-2021</b></font></h2>
+           <h2><font color="#f8f8ff">Covid-19 Active Cases <b>568</b> Updated: <b>30-05-2021</b></font></h2>
          </Paper>
    </Control>
    <Control position="bottomright">
        <Paper style={{padding: '20px'},{backgroundColor: fade('#484848', 0.5)}}>
-       <font color="#f8f8ff"><h5>As of 27 May 2021</h5></font>
+       <font color="#f8f8ff"><h5>As of 30 May 2021</h5></font>
             <font color="#f8f8ff">
-              <p><b>10</b><h6> Unknown Location</h6></p>
-              <p><b>70</b><h6> New Cases</h6></p>
-              <p><b>2963</b><h6> Passed Away</h6></p>
-              <p><b>53282</b><h6> Recovered</h6></p></font>
+              <p><b>14</b><h6> Unknown Location</h6></p>
+              <p><b>184</b><h6> New Cases</h6></p>
+              <p><b>2969</b><h6> Passed Away</h6></p>
+              <p><b>53291</b><h6> Recovered</h6></p></font>
          </Paper> 
           </Control>
     </Map>
